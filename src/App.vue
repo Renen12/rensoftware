5002e262fbc5775c097e894defbcf0f3fadbb969
Olle Modin <ollemodin@outlook.com>
2024-12-03 17:22:41 +0100

    Search bar added and some improvements

--- src/App.vue
+++ src/App.vue
@@ -24,8 +24,17 @@ function refresh_apps() {
         document.body.appendChild(document.createElement("br"));
 
         button.onclick = async () => {
-
+          document.querySelectorAll("button").forEach((btn) => {
+            btn.disabled = true
+          });
+          document.getElementById("home").href = "javascript:void(0)";
+          document.getElementById("updates").href = "javascript:void(0)";
           await invoke("install_app", { application: button.internal_appid });
+          document.querySelectorAll("button").forEach((btn) => {
+            btn.disabled = false
+          });
+          document.getElementById("home").href = "/index.html";
+          document.getElementById("updates").href = "/updates.html";
           alert("Software installation has finished.")
         };
       }
@@ -34,10 +43,58 @@ function refresh_apps() {
   });
 }
 refresh_apps();
+document.getElementById("go").onclick = async () => {
+  proceedWithSearch();
+}
+document.getElementById("search").onkeydown = async (key) => {
+  if (key.key == "Enter") {
+    proceedWithSearch();
+  }
+}
+async function proceedWithSearch() {
+  let searchTerm = document.getElementById("search").value;
+  invoke("get_apps").then((apps) => {
+    // Remove previous app elements and breaks
+    document.querySelectorAll("button").forEach((btn) => {
+      if (btn.id != "go") {
+        btn.remove();
+      }
+    });
+    document.querySelectorAll("br").forEach((breakelement) => {
+      if (breakelement.id != "firstbreak") {
+        breakelement.remove();
+
+      }
+    });
+    apps.forEach((app) => {
+      if (app.split("|")[0].toLowerCase().includes(searchTerm.toLowerCase()) == false) {
+        return
+      }
+
+      const button = document.createElement("button");
+      button.type = "button";
+      button.innerHTML = app.split("|")[0];
+      button.internal_appid = app.split("|")[1];
+      document.body.appendChild(button)
+      document.body.appendChild(document.createElement("br"));
+      button.onclick = async () => {
+        document.querySelectorAll("button").forEach((btn) => {
+          btn.disabled = true
+        });
+        document.getElementById("home").href = "javascript:void(0)";
+        document.getElementById("updates").href = "javascript:void(0)";
+        await invoke("install_app", { application: button.internal_appid });
+        document.querySelectorAll("button").forEach((btn) => {
+          btn.disabled = false
+        });
+        document.getElementById("home").href = "/index.html";
+        document.getElementById("updates").href = "/updates.html";
+        alert("Software installation has finished.")
+      };
+    });
+  });
+}
 </script>
 
 <template>
-  <h1>
-    Home
-  </h1>
 </template>
